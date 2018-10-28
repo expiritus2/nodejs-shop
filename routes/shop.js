5305919fe753465937dfcbba7a6a40b94a0ec34f
Michail <mdrazdovich@blazemeter.com>
2018-10-28 23:48:57 +0300

    dinamic url

--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,21 @@
 const path = require('path');
-const rootDir = require('../util/path');
+
 const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
-const adminData = require('./admin');
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Products',
-        hasProducts: products.length > 0,
-        activeShop: true
-    });
+router.get('/', shopController.getIndex);
 
-});
+router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
